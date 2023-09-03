a0ddd6e7a583fcbfdf391b4ba996359350d61fa7
Shun_Yukkuri_ <113278727+Shun-Yukkuri@users.noreply.github.com>
2023-09-03 14:34:34 +0900

    E

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -293,14 +293,14 @@ class ControlsSubState extends MusicBeatSubstate
 					FlxTween.tween(bindingBlack, {alpha: 0.6}, 0.35, {ease: FlxEase.linear});
 					add(bindingBlack);
 
-					bindingText = new FlxText(FlxG.width / 0, 160, options[curOptions[curSelected]][3] + "に対応するキーを設定", false);
-					bindingText.setFormat(Paths.font("system.ttf"), 50, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-					bindingText.borderSize = 5;
+					bindingText = new FlxText(FlxG.width / -5000, 160, options[curOptions[curSelected]][3] + "に対応するキーを設定", false);
+					bindingText.setFormat(Paths.font("system.ttf"), 50, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+					bindingText.borderSize = 3;
 					add(bindingText);
 					
 					bindingText2 = new FlxText(FlxG.width / -5000, 340, "     Escでキャンセル\n     Backspaceでキーを削除", true);
-					bindingText2.setFormat(Paths.font("system.ttf"), 50, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-					bindingText2.borderSize = 5;
+					bindingText2.setFormat(Paths.font("system.ttf"), 50, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+					bindingText2.borderSize = 3;
 					add(bindingText2);
 
 					binding = true;
